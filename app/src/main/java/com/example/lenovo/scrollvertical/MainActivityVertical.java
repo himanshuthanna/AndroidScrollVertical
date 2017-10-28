1ed77ae1e7d4c553456dac98634a3fb0af9cccf1
himanshuthanna <31863402+himanshuthanna@users.noreply.github.com>
2017-10-28 11:32:56 +0530

    Vertical Scrolling

---
+++ app/src/main/java/com/example/lenovo/scrollvertical/MainActivityVertical.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.scrollvertical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityVertical extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_vertical);
+    }
+}
